d3a3c15156924488288fea551e09f2f45dff2e89
Mutaz Nassar <32297719+mutazn@users.noreply.github.com>
2022-11-03 10:44:07 +0300

    Update abc.sh

--- abc.sh
+++ abc.sh
@@ -2,5 +2,5 @@
  
 while true; do
 sleep 1
-date>> nodename.txt 2>&1;nc -zv -w 1 10.177.103.37 6432>> nodename.txt 2>&1
+date>> "$NODE_NAME ".txt 2>&1;nc -zv -w 1 10.177.103.37 6432>> "$NODE_NAME ".txt 2>&1
 done
